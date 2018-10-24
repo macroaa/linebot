ff8c4ed975930c48d0c47bba85bf53f266cf5fea
macroaa <44141486+macroaa@users.noreply.github.com>
2018-10-24 17:48:14 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,9 +43,8 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text='Nice to meet you!')
-    message2 = TextSendMessage(text=(event.source.user_id)) 
-    line_bot_api.reply_message(event.reply_token, message2)  
+    message = TextSendMessage(text=(event.source.user_id)) 
+    line_bot_api.reply_message(event.reply_token, message)  
     
    
 
